f26c13e3e8ae203675564bfb249a7e450f2fa91f
ZaidMujahid <zaidmujahid274@gmail.com>
2023-08-15 00:54:11 +0530

    "trying to fix modes"

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -23,8 +23,8 @@ const Navbar = () => {
                 <a href="#about" className="p-2 pr-6">About</a>
                 <a href="#projects" className="p-2 pr-6">Projects</a>
                 <a href="#skills" className="p-2 pr-6">Skills</a>
-                {dark && <button onClick={() => setDark(false) ? setTheme('light'): setTheme("dark")} className="p-2 pr-6 text-xl pt-1.5"><CiLight/></button>}
-                {!dark && <button onClick={() => setDark(true) ? setTheme('dark'): setTheme("light")} className="p-2 pr-6 text-xl pt-1.5"><MdDarkMode/></button>}
+                {dark && <button onClick={() => setDark(false) ? setTheme('dark'): setTheme("light")} className="p-2 pr-6 text-xl pt-1.5"><CiLight/></button>}
+                {!dark && <button onClick={() => setDark(true) ? setTheme('light'): setTheme("dark")} className="p-2 pr-6 text-xl pt-1.5"><MdDarkMode/></button>}
             </div>}
             {!nav && <div className="flex text-lg font-semibold p-2 flex-col mr-[550px] mt-2">
                 {/* <a href="#home" className='text-3xl font-bold pb-4' onClick={() => setNav(true)}>ZAID</a> */}
